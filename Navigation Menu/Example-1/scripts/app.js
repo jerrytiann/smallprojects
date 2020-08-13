2942ecc1d38298dd3ec228eca0d547a3fd68d19b
jerrytiann <jerrytiann@gmail.com>
2020-08-13 14:31:46 +1000

    added js animation for toggling the side bar

--- Navigation Menu/Example-1/scripts/app.js
+++ Navigation Menu/Example-1/scripts/app.js
@@ -1,14 +1,30 @@
 const navSlide = () => {
     const mobile_nav = document.querySelector('.mobile-nav');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-    mobile_nav.addEventListener('click', ()=>{
+
+    mobile_nav.addEventListener('click', () => {
+        //Toggle Nav
         nav.classList.toggle('nav-active');
+
+
+        //Animate links
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
     });
+
+
 }
 
 
-const app = ()=> {
+const app = () => {
     navSlide();
 }
 
